c8fef417848d7ec7ea0f86bd090cc6cfd1634430
Krystian Madra <madrakrystian@gmail.com>
2019-05-07 15:45:37 +0200

    Add Call class

---
+++ backend/src/main/java/org/brapi_igpas/api/calls/Call.java
@@ -0,0 +1,127 @@
+package org.brapi_igpas.api.calls;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Call {
+    public static final String DATATYPE_JSON = "application/json";
+    public static final String DATATYPE_CSV = "text/csv";
+
+    public static final String METHOD_GET = "GET";
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_PUT = "PUT";
+    public static final String METHOD_DELETE = "DELETE";
+
+    public static final String VERSION_ONE_ZERO = "1.0";
+    public static final String VERSION_ONE_ONE = "1.1";
+    public static final String VERSION_ONE_TWO = "1.2";
+    public static final String VERSION_ONE_THREE = "1.3";
+
+    private String call;
+    private List<String> dataTypes;
+    private List<String> methods;
+    private List<String> versions;
+
+    public Call(){
+        dataTypes = new ArrayList<>();
+        methods = new ArrayList<>();
+        versions = new ArrayList<>();
+    }
+
+    public Call(String call) {
+        this();
+        this.call = call;
+    }
+
+    public Call withDataTypeJson() {
+        return addDataType(DATATYPE_JSON);
+    }
+
+    public Call withDataTypeCsv() {
+        return addDataType(DATATYPE_CSV);
+    }
+
+    private Call addDataType(String dataType) {
+        dataTypes.add(dataType);
+        return this;
+    }
+
+    public Call withMethodGet() {
+        return addMethod(METHOD_GET);
+    }
+
+    public Call withMethodPost() {
+        return addMethod(METHOD_POST);
+    }
+
+    public Call withMethodPut() {
+        return addMethod(METHOD_PUT);
+    }
+
+    public Call withMethodDelete() {
+        return addMethod(METHOD_DELETE);
+    }
+
+    private Call addMethod(String method) {
+        methods.add(method);
+        return this;
+    }
+
+    public Call withVersionOneZero() {
+        return addVersion(VERSION_ONE_ZERO);
+    }
+
+    public Call withVersionOneOne() {
+        return addVersion(VERSION_ONE_ONE);
+    }
+
+    public Call withVersionOneTwo() {
+        return addVersion(VERSION_ONE_TWO);
+    }
+
+    public Call withVersionOneThree() { return addVersion(VERSION_ONE_THREE); }
+
+    private Call addVersion(String version) {
+        versions.add(version);
+        return this;
+    }
+
+    public String getCall() {
+        return call;
+    }
+
+    public void setCall(String call) {
+        this.call = call;
+    }
+
+    public List<String> getDataTypes() {
+        return dataTypes;
+    }
+
+    public void setDataTypes(List<String> dataTypes) {
+        this.dataTypes = dataTypes;
+    }
+
+    public List<String> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<String> methods) {
+        this.methods = methods;
+    }
+
+    public List<String> getVersions() {
+        return versions;
+    }
+
+    public void setVersions(List<String> versions) {
+        this.versions = versions;
+    }
+
+    @Override
+    public String toString() {
+        return "Call{" +
+                "call='" + call + '\'' +
+                '}';
+    }
+}
