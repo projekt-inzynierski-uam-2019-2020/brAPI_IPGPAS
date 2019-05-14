94fed4d0c9a94038cb10a0e63d3f85a4754a47c3
verbatim144 <vlad061998@gmail.com>
2019-05-14 23:38:24 +0200

    Add login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import {AppRouting} from './app-routing';
+import {routing} from './app-routing';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidebarComponent,
-    LandingPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
+    CommonModule,
+    routing,
     BrowserModule,
-    AppRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
