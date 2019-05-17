53cd11215d1525c8772ea944694e5d2b6e16f690
Krystian Madra <madrakrystian@gmail.com>
2019-05-17 11:45:43 +0200

    Update CommoncropnamesController to return BrApiDetailPayloadResponse

--- backend/src/main/java/org/brapi_igpas/api/crops/CommoncropnamesController.java
+++ backend/src/main/java/org/brapi_igpas/api/crops/CommoncropnamesController.java
@@ -1,12 +1,11 @@
 package org.brapi_igpas.api.crops;
 
+import org.brapi_igpas.api.BrApiDetailPayloadResponse;
 import org.brapi_igpas.api.exceptions.InvalidPageSizeValueException;
 import org.brapi_igpas.api.exceptions.InvalidPageValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/brapi/v1")
@@ -20,7 +19,7 @@ public class CommoncropnamesController {
     @GetMapping("/commoncropnames")
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody
-    List<String> getAll(
+    BrApiDetailPayloadResponse getAll(
             @RequestParam(value = "page", defaultValue = "0") final int page,
             @RequestParam(value = "pageSize", defaultValue = "1000") final int pageSize){
         if (page < 0){ throw new InvalidPageValueException(); }
