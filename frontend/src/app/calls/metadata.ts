9f523394bb4efb3ceda63a5defef6c700b027562
verbatim144 <vlad061998@gmail.com>
2019-05-17 11:08:09 +0200

    Add metadata class

---
+++ frontend/src/app/calls/metadata.ts
@@ -0,0 +1,9 @@
+import {Pagination} from './pagination';
+import {Status} from './status';
+import {Datafiles} from './datafiles';
+
+export class Metadata {
+  pagination: Pagination;
+  datafiles: Datafiles;
+  status: Status;
+}
