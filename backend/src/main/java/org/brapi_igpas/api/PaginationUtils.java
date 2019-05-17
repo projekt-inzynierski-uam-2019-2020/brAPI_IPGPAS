d6478ee3015bc7761288b60c06edb3a31d433d12
Krystian Madra <madrakrystian@gmail.com>
2019-05-17 11:47:09 +0200

    Update PaginationUtils class

--- backend/src/main/java/org/brapi_igpas/api/PaginationUtils.java
+++ backend/src/main/java/org/brapi_igpas/api/PaginationUtils.java
@@ -1,6 +1,32 @@
 package org.brapi_igpas.api;
 
+import org.brapi_igpas.api.metadata.Pagination;
+
 public class PaginationUtils {
+
+    public static Pagination getPaginationInfo(int elementsSize, int page, int pageSize){
+        int totalPages = getTotalPages(elementsSize, pageSize);
+        return new Pagination(page, pageSize, elementsSize, totalPages);
+    }
+
+    public static int getNumberOfElementsOnCurrentPage(int elementsSize, int page, int pageSize){
+        if(page * pageSize >= elementsSize){
+            return 0;
+        }
+        int totalPages = getTotalPages(elementsSize, pageSize);
+
+        int fromIndex = 0;
+        if (page < totalPages) {
+            fromIndex = getFromIndex(elementsSize, page, pageSize);
+        }
+        return Math.min(pageSize, elementsSize - fromIndex);
+    }
+
+    private static int getTotalPages(int elementsSize, int pageSize){
+        final int zeroPage = 1;
+        int totalPages = (int) Math.ceil(elementsSize / pageSize);
+        return  totalPages == 0 ? totalPages + zeroPage : totalPages;
+    }
 
     public static int getFromIndex(int elementsSize, int page, int pageSize){
         if(page * pageSize >= elementsSize){
@@ -9,16 +35,4 @@ public class PaginationUtils {
         else return page * pageSize;
     }
 
-    public static int getNumberOfElementsOnCurrentPage(int elementsSize, int page, int pageSize){
-        if(page * pageSize >= elementsSize){
-            return 0;
-        }
-        int totalPages = (int) Math.ceil(elementsSize / pageSize);
-
-        int fromIndex = 0;
-        if (page < totalPages) {
-            fromIndex = getFromIndex(elementsSize, page, pageSize);
-        }
-        return Math.min(pageSize, elementsSize - fromIndex);
-    }
 }
