cecf57503558c5a13c0ef07202f2115af612a007
Krystian Madra <madrakrystian@gmail.com>
2019-05-17 11:52:42 +0200

    Update api Call classes to BrApi response

--- backend/src/main/java/org/brapi_igpas/api/calls/CallController.java
+++ backend/src/main/java/org/brapi_igpas/api/calls/CallController.java
@@ -1,12 +1,11 @@
 package org.brapi_igpas.api.calls;
 
+import org.brapi_igpas.api.BrApiDetailPayloadResponse;
 import org.brapi_igpas.api.exceptions.InvalidPageSizeValueException;
 import org.brapi_igpas.api.exceptions.InvalidPageValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/brapi/v1")
@@ -19,7 +18,8 @@ public class CallController {
 
     @GetMapping("/calls")
     @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody List<Call> getAll(
+    public @ResponseBody
+    BrApiDetailPayloadResponse getAll(
             @RequestParam(value = "dataType", required=false) final String dataType,
             @RequestParam(value = "page", defaultValue = "0") final int page,
             @RequestParam(value = "pageSize", defaultValue = "1000") final int pageSize){
--- backend/src/main/java/org/brapi_igpas/api/calls/CallDao.java
+++ backend/src/main/java/org/brapi_igpas/api/calls/CallDao.java
@@ -1,6 +1,8 @@
 package org.brapi_igpas.api.calls;
 
+import org.brapi_igpas.api.BrApiDetailPayloadResponse;
 import org.brapi_igpas.api.PaginationUtils;
+import org.brapi_igpas.api.metadata.Pagination;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -26,7 +28,7 @@ public class CallDao {
                 .withVersionOneTwo()
                 .withVersionOneThree());
     }
-
+    /*
     public List<Call> getAll(String dataType, int page, int pageSize){
         List<Call> calls = CALLS;
 
@@ -46,4 +48,30 @@ public class CallDao {
 
         return calls;
     }
+    */
+
+
+    public BrApiDetailPayloadResponse getAll(String dataType, int page, int pageSize){
+        List<Call> calls = CALLS;
+        Pagination paginationInfo = PaginationUtils.getPaginationInfo(calls.size(), page, pageSize);
+
+        if(dataType != null && !dataType.isEmpty()){
+            calls = calls.stream()
+                    .filter(c -> c.getDataTypes().contains(dataType))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+
+        int fromIndex = PaginationUtils.getFromIndex(calls.size(), page, pageSize);
+        int numberOfCalls = PaginationUtils.getNumberOfElementsOnCurrentPage(calls.size(), page, pageSize);
+
+        calls = calls
+                .subList(fromIndex, calls.size())
+                .stream()
+                .limit(numberOfCalls)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+
+        return new BrApiDetailPayloadResponse(calls, paginationInfo);
+    }
+
 }
