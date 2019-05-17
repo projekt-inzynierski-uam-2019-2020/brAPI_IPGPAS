60ca3fea3d478ece7511dcbddadf2cf83befb1c3
Krystian Madra <madrakrystian@gmail.com>
2019-05-17 11:49:11 +0200

    Add Status class

---
+++ backend/src/main/java/org/brapi_igpas/api/metadata/Status.java
@@ -0,0 +1,25 @@
+package org.brapi_igpas.api.metadata;
+
+public class Status {
+    private String message;
+    private String messageType;
+
+    public Status() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+}
