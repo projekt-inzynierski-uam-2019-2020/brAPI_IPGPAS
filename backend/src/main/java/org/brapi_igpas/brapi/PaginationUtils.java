d8d6cc46cd54dcf35306d095cfce706f3944c12f
Krystian Madra <madrakrystian@gmail.com>
2019-05-18 16:16:47 +0200

    Update and refactor PaginationUtils class

--- backend/src/main/java/org/brapi_igpas/brapi/PaginationUtils.java
+++ backend/src/main/java/org/brapi_igpas/brapi/PaginationUtils.java
@@ -4,35 +4,35 @@ import org.brapi_igpas.brapi.metadata.Pagination;
 
 public class PaginationUtils {
 
-    public static Pagination getPaginationInfo(int elementsSize, int page, int pageSize){
-        int totalPages = getTotalPages(elementsSize, pageSize);
-        return new Pagination(page, pageSize, elementsSize, totalPages);
+    public static Pagination getPaginationInfo(int totalCount, int page, int pageSize) {
+        int totalPages = getTotalPages(totalCount, pageSize);
+        return new Pagination(page, pageSize, totalCount, totalPages);
     }
 
-    public static int getNumberOfElementsOnCurrentPage(int elementsSize, int page, int pageSize){
-        if(page * pageSize >= elementsSize){
+    public static int getFromIndex(int elementsSize, int page, int pageSize) {
+        final int firstIndex = 0;
+        if (isFromIndexInPaginationBorders(elementsSize, page, pageSize)) {
+            return page * pageSize;
+        } else return firstIndex;
+    }
+
+    public static int getToIndex(int elementsSize, int page, int pageSize) {
+        if (!isFromIndexInPaginationBorders(elementsSize, page, pageSize)) {
             return 0;
         }
-        int totalPages = getTotalPages(elementsSize, pageSize);
+        int fromIndex = getFromIndex(elementsSize, page, pageSize);
+        int numberOfElements = Math.min(pageSize, elementsSize - fromIndex);
 
-        int fromIndex = 0;
-        if (page < totalPages) {
-            fromIndex = getFromIndex(elementsSize, page, pageSize);
-        }
-        return Math.min(pageSize, elementsSize - fromIndex);
+        return fromIndex + numberOfElements;
     }
 
-    private static int getTotalPages(int elementsSize, int pageSize){
+    private static boolean isFromIndexInPaginationBorders(int elementsSize, int page, int pageSize) {
+        return page * pageSize < elementsSize;
+    }
+
+    private static int getTotalPages(int elementsSize, int pageSize) {
         final int zeroPage = 1;
         int totalPages = (int) Math.ceil(elementsSize / pageSize);
-        return  totalPages == 0 ? totalPages + zeroPage : totalPages;
+        return totalPages == 0 ? totalPages + zeroPage : totalPages;
     }
-
-    public static int getFromIndex(int elementsSize, int page, int pageSize){
-        if(page * pageSize >= elementsSize){
-            return 0;
-        }
-        else return page * pageSize;
-    }
-
 }
