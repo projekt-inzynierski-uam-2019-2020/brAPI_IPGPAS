632b836fdca26c63d53520cd9d72576b1f41a843
Vladyslav Liaskovskyi <s426177@labs.wmi.amu.edu.pl>
2019-05-21 17:52:36 +0200

    Add additionalInfo class and update navbar fonts

---
+++ frontend/src/app/calls/studies/additionalInfo.ts
@@ -0,0 +1,4 @@
+export class AdditionalInfo {
+  studyObjective: string;
+  publications: string;
+}
--- frontend/src/app/calls/studies/studies.ts
+++ frontend/src/app/calls/studies/studies.ts
@@ -1,7 +1,22 @@
+import {AdditionalInfo} from './additionalInfo';
+
 export class Studies {
   active: boolean;
+  additionalInfo: AdditionalInfo;
   commonCropName: string;
+  documentationURL: string;
+  endDate: string;
+  locationDbId: string;
+  locationName: string;
   name: string;
+  programDbId: string;
+  programName: string;
   startDate: string;
+  studyDbId: string;
+  studyName: string;
+  studyType: string;
+  studyTypeDbId: string;
   studyTypeName: string;
+  trialDbId: string;
+  trialName: string;
 }
