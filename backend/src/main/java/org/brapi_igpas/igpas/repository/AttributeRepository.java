7b91df737f10ff539b499cafbaeb84ab8ac8cd5b
Krystian Madra <madrakrystian@gmail.com>
2019-05-28 11:39:36 +0200

    Update AttributeRepository interface

--- backend/src/main/java/org/brapi_igpas/igpas/repository/AttributeRepository.java
+++ backend/src/main/java/org/brapi_igpas/igpas/repository/AttributeRepository.java
@@ -3,6 +3,8 @@ package org.brapi_igpas.igpas.repository;
 import org.brapi_igpas.igpas.entity.Attribute;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AttributeRepository extends CrudRepository<Attribute, Integer> {
-    Attribute getAttributeByDisplayedName(String displayedName);
+    Optional<Attribute> getAttributeByDisplayedName(String displayedName);
 }
