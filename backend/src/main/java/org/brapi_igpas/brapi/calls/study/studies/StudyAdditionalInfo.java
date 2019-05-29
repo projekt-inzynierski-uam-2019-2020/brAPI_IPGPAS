185ced747e585bccb388d30ad300c43b2bba5edc
Krystian Madra <madrakrystian@gmail.com>
2019-05-29 20:16:37 +0200

    Add StudyAdditionalInfo class

---
+++ backend/src/main/java/org/brapi_igpas/brapi/calls/study/studies/StudyAdditionalInfo.java
@@ -0,0 +1,214 @@
+package org.brapi_igpas.brapi.calls.study.studies;
+
+public class StudyAdditionalInfo {
+    private String description;
+    private String volume;
+    private String typeOfFertiliser;
+    private String sowingDensity;
+    private String plotSize;
+    private String pH;
+    private String numberOfPlantsPerContainer;
+    private String nightTemperature;
+    private String nBeforeFertilisation;
+    private String lengthOfLightPeriod;
+    private String irrigationType;
+    private String frequency;
+    private String factor;
+    private String experimentalUnit;
+    private String dayTemperature;
+    private String dailyPhotonFlux;
+    private String containerVolume;
+    private String containerType;
+    private String containerDimension;
+    private String characteristics;
+    private String assayType;
+    private String amountOfFertiliser;
+    private String airHumidity;
+
+    public StudyAdditionalInfo() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getTypeOfFertiliser() {
+        return typeOfFertiliser;
+    }
+
+    public void setTypeOfFertiliser(String typeOfFertiliser) {
+        this.typeOfFertiliser = typeOfFertiliser;
+    }
+
+    public String getSowingDensity() {
+        return sowingDensity;
+    }
+
+    public void setSowingDensity(String sowingDensity) {
+        this.sowingDensity = sowingDensity;
+    }
+
+    public String getPlotSize() {
+        return plotSize;
+    }
+
+    public void setPlotSize(String plotSize) {
+        this.plotSize = plotSize;
+    }
+
+    public String getpH() {
+        return pH;
+    }
+
+    public void setpH(String pH) {
+        this.pH = pH;
+    }
+
+    public String getNumberOfPlantsPerContainer() {
+        return numberOfPlantsPerContainer;
+    }
+
+    public void setNumberOfPlantsPerContainer(String numberOfPlantsPerContainer) {
+        this.numberOfPlantsPerContainer = numberOfPlantsPerContainer;
+    }
+
+    public String getNightTemperature() {
+        return nightTemperature;
+    }
+
+    public void setNightTemperature(String nightTemperature) {
+        this.nightTemperature = nightTemperature;
+    }
+
+    public String getnBeforeFertilisation() {
+        return nBeforeFertilisation;
+    }
+
+    public void setnBeforeFertilisation(String nBeforeFertilisation) {
+        this.nBeforeFertilisation = nBeforeFertilisation;
+    }
+
+    public String getLengthOfLightPeriod() {
+        return lengthOfLightPeriod;
+    }
+
+    public void setLengthOfLightPeriod(String lengthOfLightPeriod) {
+        this.lengthOfLightPeriod = lengthOfLightPeriod;
+    }
+
+    public String getIrrigationType() {
+        return irrigationType;
+    }
+
+    public void setIrrigationType(String irrigationType) {
+        this.irrigationType = irrigationType;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getFactor() {
+        return factor;
+    }
+
+    public void setFactor(String factor) {
+        this.factor = factor;
+    }
+
+    public String getExperimentalUnit() {
+        return experimentalUnit;
+    }
+
+    public void setExperimentalUnit(String experimentalUnit) {
+        this.experimentalUnit = experimentalUnit;
+    }
+
+    public String getDayTemperature() {
+        return dayTemperature;
+    }
+
+    public void setDayTemperature(String dayTemperature) {
+        this.dayTemperature = dayTemperature;
+    }
+
+    public String getDailyPhotonFlux() {
+        return dailyPhotonFlux;
+    }
+
+    public void setDailyPhotonFlux(String dailyPhotonFlux) {
+        this.dailyPhotonFlux = dailyPhotonFlux;
+    }
+
+    public String getContainerVolume() {
+        return containerVolume;
+    }
+
+    public void setContainerVolume(String containerVolume) {
+        this.containerVolume = containerVolume;
+    }
+
+    public String getContainerType() {
+        return containerType;
+    }
+
+    public void setContainerType(String containerType) {
+        this.containerType = containerType;
+    }
+
+    public String getContainerDimension() {
+        return containerDimension;
+    }
+
+    public void setContainerDimension(String containerDimension) {
+        this.containerDimension = containerDimension;
+    }
+
+    public String getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+
+    public String getAssayType() {
+        return assayType;
+    }
+
+    public void setAssayType(String assayType) {
+        this.assayType = assayType;
+    }
+
+    public String getAmountOfFertiliser() {
+        return amountOfFertiliser;
+    }
+
+    public void setAmountOfFertiliser(String amountOfFertiliser) {
+        this.amountOfFertiliser = amountOfFertiliser;
+    }
+
+    public String getAirHumidity() {
+        return airHumidity;
+    }
+
+    public void setAirHumidity(String airHumidity) {
+        this.airHumidity = airHumidity;
+    }
+}
