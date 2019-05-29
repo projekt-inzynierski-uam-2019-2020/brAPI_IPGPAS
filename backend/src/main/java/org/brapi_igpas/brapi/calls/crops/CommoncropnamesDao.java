7976285535156c67138972e281f4801f4950ce18
Krystian Madra <madrakrystian@gmail.com>
2019-05-29 20:08:49 +0200

    Add CommoncropnamesDao interface layer

--- backend/src/main/java/org/brapi_igpas/brapi/calls/crops/CommoncropnamesDao.java
+++ backend/src/main/java/org/brapi_igpas/brapi/calls/crops/CommoncropnamesDao.java
@@ -1,40 +1,9 @@
 package org.brapi_igpas.brapi.calls.crops;
 
 import org.brapi_igpas.brapi.BrApiDetailPayloadResponse;
-import org.brapi_igpas.brapi.PaginationUtils;
-import org.brapi_igpas.brapi.metadata.Pagination;
-import org.brapi_igpas.igpas.entity.Value;
-import org.brapi_igpas.igpas.service.DbValuesFacade;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+public interface CommoncropnamesDao {
+    BrApiDetailPayloadResponse getAll(int page, int pageSize);
 
-@Repository
-public class CommoncropnamesDao {
-    private final DbValuesFacade dbValuesFacade;
-
-    @Autowired
-    public CommoncropnamesDao(DbValuesFacade dbValuesFacade) {
-        this.dbValuesFacade = dbValuesFacade;
-    }
-
-    public BrApiDetailPayloadResponse getAll(int page, int pageSize) {
-        List<Value> values = dbValuesFacade.getAllValuesByAttributeDisplayedName("Organism");
-
-        List<String> commonCropNames = getValuesNames(values);
-
-        Pagination paginationInfo = PaginationUtils.getPaginationInfo(commonCropNames.size(), page, pageSize);
-        int fromIndex = PaginationUtils.getFromIndex(commonCropNames.size(), page, pageSize);
-        int toIndex = PaginationUtils.getToIndex(commonCropNames.size(), page, pageSize);
-        commonCropNames = commonCropNames.subList(fromIndex, toIndex);
-
-        return new BrApiDetailPayloadResponse(commonCropNames, paginationInfo);
-    }
-
-    private List<String> getValuesNames(List<Value> values) {
-        return values.stream().map(Value::getValue).distinct().collect(Collectors.toCollection(ArrayList::new));
-    }
+    String getCommonCropNameForStudyWithStudyId(long studyId);
 }
