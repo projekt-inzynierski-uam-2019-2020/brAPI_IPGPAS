b4035cbf2688713877eca76feb1f2a75a60f83b9
Krystian Madra <madrakrystian@gmail.com>
2019-05-29 20:18:14 +0200

    Update CallDao class tests

--- backend/src/test/java/org/brapi_igpas/brapi/calls/calls/CallDaoTest.java
+++ backend/src/test/java/org/brapi_igpas/brapi/calls/calls/CallDaoTest.java
@@ -2,6 +2,7 @@ package org.brapi_igpas.brapi.calls.calls;
 
 import org.brapi_igpas.brapi.BrApiDetailPayloadResponse;
 import org.brapi_igpas.brapi.metadata.Pagination;
+import org.brapi_igpas.brapi.result.Result;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,12 +21,10 @@ public class CallDaoTest {
 
     private CallDao callDao;
     private List<Call> calls;
-    private Call callWithCsvType;
 
     @Before
     public void init() {
         calls = new ArrayList<>();
-
         calls.add(new Call("calls")
                 .withDataTypeJson()
                 .withMethodGet()
@@ -40,29 +39,33 @@ public class CallDaoTest {
                 .withVersionOneTwo()
                 .withVersionOneThree());
 
-        callWithCsvType = new Call("Third").withDataTypeCsv();
-        calls.add(callWithCsvType);
-
-        callDao = new CallDao(calls);
+        callDao = new CallDao();
+        callDao.CALLS = calls;
     }
 
     @Test
     public void getAllShouldReturnBrApiDetailPayloadResponseWithAllCalls() {
-        assertEquals(new BrApiDetailPayloadResponse(calls, new Pagination(0,0,0,0)).getResult().getData(), callDao.getAll(dataType, page, pageSize).getResult().getData());
+        Result result = new Result(calls);
+        assertEquals(result.getData(), callDao.getAll(dataType, page, pageSize).getResult().getData());
     }
 
     @Test
     public void getAllShouldReturnBrApiDetailPayloadResponseWithCallsWithGivenDataType() {
-        assertEquals(new BrApiDetailPayloadResponse(Collections.singletonList(callWithCsvType), new Pagination(0,0,0,0)).getResult().getData(), callDao.getAll("text/csv", page, pageSize).getResult().getData());
+        Call callWithCsvType = new Call("Lorem ipsum").withDataTypeCsv().withMethodDelete();
+        calls.add(callWithCsvType);
+        Result result = new Result(Collections.singletonList(callWithCsvType));
+        assertEquals(result.getData(), callDao.getAll("text/csv", page, pageSize).getResult().getData());
     }
 
     @Test
     public void getAllShouldReturnBrApiDetailPayloadResponseWithCallsFromGivenPage() {
-        assertEquals(new BrApiDetailPayloadResponse(calls.subList(1,2), new Pagination(0,0,0,0)).getResult().getData(), callDao.getAll(dataType, 1, 1).getResult().getData());
+        Result result = new Result(calls.subList(1, 2));
+        assertEquals(result.getData(), callDao.getAll(dataType, 1, 1).getResult().getData());
     }
 
     @Test
     public void getAllShouldReturnBrApiDetailPayloadResponseWithEmptyList() {
-        assertEquals(new BrApiDetailPayloadResponse(Collections.emptyList(), new Pagination(0,0,0,0)).getResult().getData(), callDao.getAll(dataType, 5, 50).getResult().getData());
+        Result result = new Result(Collections.emptyList());
+        assertEquals(result.getData(), callDao.getAll(dataType, 5, 50).getResult().getData());
     }
 }
