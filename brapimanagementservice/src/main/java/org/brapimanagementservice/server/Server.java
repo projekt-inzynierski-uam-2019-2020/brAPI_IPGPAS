219b6a87aef738d5f93d9134d5008988c49a127b
Szymon Wojciechowski <szywoj95@gmail.com>
2019-05-30 12:52:26 +0200

    added server class

---
+++ brapimanagementservice/src/main/java/org/brapimanagementservice/server/Server.java
@@ -0,0 +1,26 @@
+package org.brapimanagementservice.server;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Server {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+}
