f91dff0dab59648c1436006ac1bd24af2a0aba9b
verbatim144 <vlad061998@gmail.com>
2019-05-31 00:25:57 +0200

    Add new server card to component

--- frontend/src/app/calls/calls.service.ts
+++ frontend/src/app/calls/calls.service.ts
@@ -12,13 +12,7 @@ export class CallsService {
   constructor(private http: HttpClient) {
   }
 
-  private callsUrl = 'https://test-server.brapi.org/brapi/v1/calls';
   private url = 'https://test-server.brapi.org/brapi/v1';
-
-
-  public getCalls() {
-    return this.http.get<BrApiDetailPayloadResponse>(this.callsUrl);
-  }
 
   public getSelectedCall(selectedItem: string) {
     return this.http.get<BrApiDetailPayloadResponse>(this.url + '/' + selectedItem);
