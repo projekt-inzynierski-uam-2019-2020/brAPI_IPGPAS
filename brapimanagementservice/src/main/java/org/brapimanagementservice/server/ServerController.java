73deca88359649d36669663b0304d705b552c739
Szymon Wojciechowski <szywoj95@gmail.com>
2019-05-30 23:11:20 +0200

    added more mappings

--- brapimanagementservice/src/main/java/org/brapimanagementservice/server/ServerController.java
+++ brapimanagementservice/src/main/java/org/brapimanagementservice/server/ServerController.java
@@ -1,8 +1,7 @@
 package org.brapimanagementservice.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,14 +9,48 @@ import java.util.List;
 @RequestMapping("/api/v1/servers")
 public class ServerController {
 
-    private ServerRepository repo;
+    private ServerRepository serverRepository;
 
     ServerController(ServerRepository serverRepository) {
-        this.repo = serverRepository;
+        this.serverRepository = serverRepository;
     }
 
     @GetMapping
     public List findAll() {
-        return repo.findAll();
+        return serverRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Server> findById(@PathVariable long id) {
+        return serverRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Server create(@RequestBody Server server) {
+        return serverRepository.save(server);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Server> update(@PathVariable long id, @RequestBody Server server) {
+        return serverRepository.findById(id)
+                .map(record -> {
+                    record.setName(server.getName());
+                    record.setDescription(server.getDescription());
+                    record.setAddress(server.getAddress());
+                    Server updated = serverRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return serverRepository.findById(id)
+                .map(record -> {
+                    serverRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
 }
