a1c622d0d993e37f688a4d821eba16d2d92ceb65
verbatim144 <vlad061998@gmail.com>
2019-06-01 23:32:22 +0200

    Add canvas functions to study component

--- frontend/src/app/calls-components/study/study.component.ts
+++ frontend/src/app/calls-components/study/study.component.ts
@@ -3,12 +3,15 @@ import {CallsService} from '../../calls/calls.service';
 import {BrApiDetailPayloadResponse} from '../../calls/BrApiDetailPayloadResponse';
 import {Chart} from 'chart.js';
 import {Server} from '../../calls/server';
-import {Subscription} from 'rxjs';
+
+import {ChartService} from '../../services/chart-service/chart-service';
+
 
 @Component({
   selector: 'app-study',
   templateUrl: './study.component.html',
-  styleUrls: ['./study.component.css']
+  styleUrls: ['./study.component.css'],
+  providers: [ChartService],
 
 })
 export class StudyComponent implements OnInit {
@@ -17,16 +20,9 @@ export class StudyComponent implements OnInit {
   cropNames: string[] = [];
   sumOfCropNames: number[] = [];
   marked: false;
-  LineChart = [];
-  BarChart = [];
   canvasShow = false;
-  backgroundChartColor = [];
-  backgroundBorderChartColor = [];
-  arrayOfRgba: string[] = [];
-  arrayOfRgbaBorder: string[] = [];
-  private messageSubscription: Subscription;;
 
-  constructor(private callsService: CallsService, private server: Server) {
+  constructor(private callsService: CallsService, private server: Server, private chartService: ChartService) {
   }
 
   ngOnInit() {
@@ -66,80 +62,13 @@ export class StudyComponent implements OnInit {
     this.canvasShow = true;
   }
 
-  columnChart() {
-    this.backgroundChartColor = ['rgba(255, 99, 132, 0.3)', 'rgba(54, 162, 235, 0.3)', 'rgba(255, 206, 86, 0.3)', 'rgba(29, 255, 140, 0.3)', 'rgba(215, 117, 44, 0.3)', 'rgba(7, 19, 157, 0.3)', 'rgba(239, 5, 0, 0.3)', 'rgba(243, 216, 31, 0.3)', 'rgba(200, 145, 31, 0.3)', 'rgba(215, 117, 44, 0.3)'];
-    this.backgroundBorderChartColor = [' rgba(255, 99, 132, 1)', 'rgba(54, 162, 235,1)', 'rgba(255, 206, 86, 1)', 'rgba(29, 255, 140, 1)', 'rgba(215, 117, 44, 1)', 'rgba(7, 19, 157, 1)', 'rgba(239, 5, 0, 1)', 'rgba(243, 216, 31, 1)', 'gba(200, 145, 31, 1)', 'rgba(215, 117, 44, 1)'];
-    for (let i = 0; i < this.cropNames.length; i++) {
-      if (i > this.backgroundChartColor.length) {
-        this.arrayOfRgba[i] = this.backgroundChartColor[i % 10];
-        this.arrayOfRgbaBorder[i] = this.backgroundBorderChartColor[i % 10];
-      } else {
-        this.arrayOfRgba[i] = this.backgroundChartColor[i];
-        this.arrayOfRgbaBorder[i] = this.backgroundBorderChartColor[this.arrayOfRgba[i]];
-      }
-    }
-    this.BarChart = new Chart('barChart', {
-      type: 'bar',
-      data: {
-        labels: this.cropNames,
-        datasets: [{
-          label: '# of Items',
-          data: this.sumOfCropNames,
-          backgroundColor: this.arrayOfRgba,
-          borderColor: this.arrayOfRgbaBorder,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        title: {
-          text: '# of CommonCropName in Studies',
-          fontFamily: 'Verdana',
-          fontSize: 15,
-          fontStyle: 'normal',
-          display: true
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+  showColumnChart() {
+    this.chartService.chartColumnStyle(this.cropNames);
+    this.chartService.columnChart(this.cropNames, this.sumOfCropNames);
   }
 
-  lineChart() {
-    this.LineChart = new Chart('lineChart', {
-      type: 'line',
-      data: {
-        labels: this.cropNames,
-        datasets: [{
-          label: '# of Items',
-          data: this.sumOfCropNames,
-          fill: false,
-          lineTension: 0.2,
-          borderColor: 'red',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        title: {
-          text: '# of CommonCropName in Studies',
-          fontFamily: 'Verdana',
-          fontSize: 15,
-          fontStyle: 'normal',
-          display: true
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+  showLineChart() {
+    this.chartService.lineChart(this.cropNames, this.sumOfCropNames);
   }
 
   getCallsLength() {
@@ -153,8 +82,8 @@ export class StudyComponent implements OnInit {
 
   toggleVisibility(e) {
     this.marked = e.target.checked;
-    this.lineChart();
-    this.columnChart();
+    this.showLineChart()
+    this.showColumnChart()
     if (!this.marked) {
       this.canvasShow = false;
     }
