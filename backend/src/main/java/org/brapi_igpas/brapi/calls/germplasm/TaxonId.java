3680288d8ac3c703c3421a5c291b81144db219f4
Krystian Madra <madrakrystian@gmail.com>
2019-06-02 15:06:25 +0200

    Add equals and hashcode to TaxonId class

--- backend/src/main/java/org/brapi_igpas/brapi/calls/germplasm/TaxonId.java
+++ backend/src/main/java/org/brapi_igpas/brapi/calls/germplasm/TaxonId.java
@@ -1,5 +1,7 @@
 package org.brapi_igpas.brapi.calls.germplasm;
 
+import java.util.Objects;
+
 public class TaxonId {
     private String sourceName;
     private String taxonId;
@@ -21,5 +23,19 @@ public class TaxonId {
 
     public void setTaxonId(String taxonId) {
         this.taxonId = taxonId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaxonId taxonId1 = (TaxonId) o;
+        return Objects.equals(sourceName, taxonId1.sourceName) &&
+                Objects.equals(taxonId, taxonId1.taxonId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sourceName, taxonId);
     }
 }
