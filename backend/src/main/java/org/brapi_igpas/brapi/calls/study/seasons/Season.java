52213eb174c7f2d1bc4f7389a6e9ff849bcabad0
Krystian Madra <madrakrystian@gmail.com>
2019-06-02 14:06:26 +0200

    Add equals and hashcode methods to Season class

--- backend/src/main/java/org/brapi_igpas/brapi/calls/study/seasons/Season.java
+++ backend/src/main/java/org/brapi_igpas/brapi/calls/study/seasons/Season.java
@@ -1,11 +1,19 @@
 package org.brapi_igpas.brapi.calls.study.seasons;
 
+import java.util.Objects;
+
 public class Season {
     private String season;
     private String seasonDbId;
     private String year;
 
-    public Season(){
+    public Season() {
+    }
+
+    public Season(String season, String seasonDbId, String year) {
+        this.season = season;
+        this.seasonDbId = seasonDbId;
+        this.year = year;
     }
 
     public String getSeason() {
@@ -30,5 +38,20 @@ public class Season {
 
     public void setYear(String year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Season season1 = (Season) o;
+        return Objects.equals(season, season1.season) &&
+                Objects.equals(seasonDbId, season1.seasonDbId) &&
+                Objects.equals(year, season1.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(season, seasonDbId, year);
     }
 }
