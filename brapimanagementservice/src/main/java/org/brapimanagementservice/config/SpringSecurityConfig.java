9600ad56af0ded02b7b084cca78aae8fcdf16e14
Szymon Wojciechowski <szywoj95@gmail.com>
2019-06-02 20:53:26 +0200

    initial spring security configuration

---
+++ brapimanagementservice/src/main/java/org/brapimanagementservice/config/SpringSecurityConfig.java
@@ -0,0 +1,20 @@
+package org.brapimanagementservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().
+                and().
+                authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/servers").permitAll().and().
+                authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/servers/*").permitAll().and().
+                authorizeRequests().anyRequest().authenticated();
+    }
+}
