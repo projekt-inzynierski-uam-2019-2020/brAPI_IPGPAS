2c7cc74060a8977b681a293dc83893eefdf2d7d0
verbatim144 <vlad061998@gmail.com>
2019-06-04 08:03:26 +0200

    Add getBase64Image function to calculate a size of canvas

--- frontend/src/app/services/pdf-service/pdf-service.ts
+++ frontend/src/app/services/pdf-service/pdf-service.ts
@@ -4,5 +4,14 @@ import * as jsPDF from 'jspdf';
 @Injectable()
 export class PdfService {
 
- 
+  getBase64Image(img) {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    const dataURL = canvas.toDataURL('image/png');
+    return dataURL;
+  }
+  
 }
