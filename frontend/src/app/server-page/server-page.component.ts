d52106f2190ea50688188605b919d9f084cd87fa
Kuba <jklasecki@gmail.com>
2019-10-12 20:13:53 +0200

    Change 'choose server' page and part of toggle implementation

--- frontend/src/app/server-page/server-page.component.ts
+++ frontend/src/app/server-page/server-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Server} from '../calls/server';
 import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {style} from '@angular/animations';
 
 
 @Component({
@@ -9,6 +10,7 @@ import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./server-page.component.scss']
 })
 export class ServerPageComponent {
+  isActive = false;
   checkboxes = [
     {
       value: 'https://test-server.brapi.org/',
@@ -33,5 +35,28 @@ export class ServerPageComponent {
       });
     console.log(result);
   }
+  public toggleStyle(id) {
+    const header = document.getElementById(`card-header${ id }`);
+    const body = document.getElementById(`card-body${ id }`);
 
+  /*  if (!isActive) {
+      isActive = true;
+    } else {
+        isActive = false;
+      }*/
+
+    if (this.isActive) {
+      header.style.backgroundColor = '#B6844A';
+      body.style.backgroundColor = '#F5E0B7';
+      this.checkboxes[id].selected = true;
+      console.log('active' + id);
+      this.isActive = !this.isActive;
+    } else {
+      header.style.backgroundColor = '#858585';
+      body.style.backgroundColor = '#FFFFFF';
+      this.checkboxes[id].selected = false;
+      console.log('non active' + id);
+      this.isActive = !this.isActive;
+    }
+  }
 }
