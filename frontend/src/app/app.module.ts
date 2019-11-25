f7316b44e656c1e37c99c21c6682b92939ac3e5a
kubal97 <jakubl1997@interia.pl>
2019-11-25 18:42:15 +0100

    Add login and signup modals

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,7 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {routing} from './app-routing';
 import {HomePageComponent} from './home-page/home-page.component';
-import {LoginPageComponent} from './login-page/login-page.component';
 import {CommonModule} from '@angular/common';
-import {RegistrationPageComponent} from './registration-page/registration-page.component';
 import {DatabasesPageComponent} from './databases-page/databases-page.component';
 import {ServerDataComponent} from './server-data-page/server-data.component';
 import {ServerPageComponent} from './server-page/server-page.component';
@@ -31,8 +29,6 @@ import {Globals} from './globals';
     NavbarComponent,
     SidebarComponent,
     HomePageComponent,
-    LoginPageComponent,
-    RegistrationPageComponent,
     ServerPageComponent,
     ServerDataComponent,
     DatabasesPageComponent,
