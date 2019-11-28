83d4d94688c2143bd4fa1d072d1c657918613861
verbatim144 <vlad061998@gmail.com>
2019-11-28 15:29:48 +0100

    Add trial service

--- frontend/src/app/call-services/trial/trial-service.ts
+++ frontend/src/app/call-services/trial/trial-service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {FetchBrapiResultService} from '../../fetch-services/fetch-brapi-result-service';
 import {Globals} from '../../globals';
 import {DetailResult} from '../../call-models/response/detailResult';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class TrialService {
@@ -16,7 +17,7 @@ export class TrialService {
   }
 
   getAllTrials(brAPIServerUrl: string) {
-    return this.fetchBrApiResponseService.getBrAPIDetailResult(brAPIServerUrl + '/brapi/v1/trials');
+    return this.fetchBrApiResponseService.getBrAPIDetailResult(brAPIServerUrl + '/brapi/v1/trials').pipe(map(result => result.data));
   }
 
   getTrialsByCommonCropName(brAPIServerUrl: string, commonCropName: string) {
