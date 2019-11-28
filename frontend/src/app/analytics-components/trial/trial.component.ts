e166abd9ded9315aec0a88b316aa0fe8e7b5ae64
verbatim144 <vlad061998@gmail.com>
2019-11-28 15:28:55 +0100

    Add trial component

--- frontend/src/app/analytics-components/trial/trial.component.ts
+++ frontend/src/app/analytics-components/trial/trial.component.ts
@@ -1,11 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {TrialService} from '../../call-services/trial/trial-service';
-import {ServersService} from '../../servers-services/servers-service';
 import {Trial} from '../../call-models/trial';
 import {Globals} from '../../globals';
-import {FetchBrapiResultService} from '../../fetch-services/fetch-brapi-result-service';
-import {ActivatedRoute, Router, Routes} from '@angular/router';
-import {StudyService} from '../../call-services/study/study-service';
+import {TrialCheckbox} from './trialCheckbox';
+import {ServerTrial} from './serverTrial';
+import {distinct} from 'rxjs/operators';
 
 @Component({
   selector: 'app-trial',
@@ -13,46 +12,46 @@ import {StudyService} from '../../call-services/study/study-service';
   styleUrls: ['./trial.component.scss']
 })
 export class TrialComponent implements OnInit {
-  checkboxes = [];
-  serversService: ServersService;
-  trialService: TrialService;
-  studyService: StudyService;
-  trials: Trial[];
   globals: Globals;
+  trialCheckboxes: TrialCheckbox[] = [];
+  serverTrials: ServerTrial[] = [];
 
-  constructor(trialService: TrialService, serversService: ServersService, globals: Globals, private fetchBrapiResult: FetchBrapiResultService, private routes: ActivatedRoute, studyService: StudyService) {
-    this.trialService = trialService;
-    this.serversService = serversService;
+  trialService: TrialService;
+
+  constructor(globals: Globals, trialService: TrialService) {
     this.globals = globals;
-    this.studyService = studyService;
+    this.trialService = trialService;
   }
 
   ngOnInit() {
-    for ( const trial of this.routes.snapshot.data.dataResolve.data){
-      this.checkboxes.push({
-        trialName: trial.trialName,
-        commonCropName: trial.commonCropName,
-        programName: trial.programName,
-        studies: trial.studies,
-        selected: false
-      });
-    }
+    this.fetchTrialsFromSelectedServers();
   }
 
-  showStudies() {
-    const resultStudies = this.checkboxes.filter((checkbox) => {
-      return checkbox.selected;
-    })
-      .map((checkbox) => {
-        this.trialService.getTrialsByCommonCropName(this.globals.selectedServer, checkbox.commonCropName);
-        return checkbox.studies;
-      });
-    for (const trials of resultStudies) {
-      for (let i = 0; i < trials.length; i++) {
-        this.studyService.getStudyByStudyDbId(this.globals.selectedServer, trials[i].studyDbId);
-        this.globals.studyDbId.push(trials[i].studyDbId);
+  fetchTrialsFromSelectedServers() {
+    this.globals.selectedServers.map(serverUrl => this.trialService.getAllTrials(serverUrl)
+      .subscribe(fetchedTrials => {
+        this.setTrialCheckboxes(fetchedTrials);
+        this.setServerTrials(serverUrl, fetchedTrials);
+      }));
+  }
+
+  setTrialCheckboxes(trials: Trial[]) {
+    trials.map(trial => this.trialCheckboxes.push({trial: trial, selected: false}));
+  }
+
+  setServerTrials(serverUrl: string, trials: Trial[]) {
+    trials.map(trial => this.serverTrials.push({serverUrl: serverUrl, trial: trial}));
+  }
+
+  setSelectedServerTrials() {
+    const selectedTrials = this.trialCheckboxes.filter(trialCheckbox => trialCheckbox.selected).map(trialCheckbox => trialCheckbox.trial);
+    this.globals.selectedServerTrials = this.serverTrials
+      .filter(serverTrial => {
+        for (const selectedTrial of selectedTrials) {
+        if (Object.is(selectedTrial, serverTrial.trial)) {
+          return true;
+        }
       }
-    }
+    });
   }
-
 }
