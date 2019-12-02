8201c1efd8563ecdcc56c5f06e04e1426701f94d
Krystian Madra <madrakrystian@gmail.com>
2019-12-02 22:04:44 +0100

    [IBDR-145] Refactor brapi commoncropnames classes

--- brapi/src/main/java/org/brapi_igpas/brapi/calls/commoncropnames/CommoncropnamesService.java
+++
@@ -1,7 +0,0 @@
-package org.brapi_igpas.brapi.calls.commoncropnames;
-
-import org.brapi_igpas.brapi.response.BrAPIDetailResponse;
-
-public interface CommoncropnamesService {
-    BrAPIDetailResponse getBrAPIDetailResponse(int page, int pageSize);
-}
