c3358d4ba19c5f8f6d153a51a05a6e6f20a7596e
verbatim144 <vlad061998@gmail.com>
2019-12-02 11:29:45 +0100

    Add germplasm checkboxes

--- frontend/src/app/analytics-components/germplasm/germplasm.component.ts
+++ frontend/src/app/analytics-components/germplasm/germplasm.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Globals} from '../../globals';
 import {GermplasmService} from '../../call-services/germplasm/germplasm-service';
+import {Germplasm} from '../../call-models/germplasm';
+import {GermplasmCheckbox} from './germplasmCheckbox';
 
 @Component({
   selector: 'app-germplasm',
@@ -11,6 +13,7 @@ export class GermplasmComponent implements OnInit {
 
   globals: Globals;
   germplasmService: GermplasmService;
+  germplasmCheckboxes: GermplasmCheckbox[] = [];
 
   constructor(globals: Globals, germplasmService: GermplasmService) {
     this.globals = globals;
@@ -26,7 +29,23 @@ export class GermplasmComponent implements OnInit {
     this.globals.selectedServerStudies.map(selectedStudy => this.germplasmService.getGermplasmByStudyDbId(selectedStudy.serverUrl, selectedStudy.study.studyDbId)
       .subscribe(fetchedGermplasm => {
         console.log(fetchedGermplasm);
+        this.setGermplasmCheckboxes(fetchedGermplasm);
       }));
   }
+
+  setGermplasmCheckboxes(germplasms: Germplasm[]) {
+
+    console.log(this.globals.selectedServerStudies);
+    for (const selectedServer of this.globals.selectedServerStudies){
+      for (const germplasm of germplasms) {
+        if (!this.germplasmCheckboxes.some((item) => item.germplasm.germplasmDbId === germplasm.germplasmDbId && item.study.studyName === selectedServer.study.studyName)) {
+          this.germplasmCheckboxes.push({germplasm: germplasm, selected: false, study: selectedServer.study});
+        }
+      }
+    }
+
+    console.log(this.germplasmCheckboxes);
+  }
+
 
 }
---
+++ frontend/src/app/analytics-components/germplasm/germplasmCheckbox.ts
@@ -0,0 +1,8 @@
+import {Germplasm} from '../../call-models/germplasm';
+import {Study} from '../../call-models/study';
+
+export class GermplasmCheckbox {
+  germplasm: Germplasm;
+  selected: false;
+  study: Study;
+}
