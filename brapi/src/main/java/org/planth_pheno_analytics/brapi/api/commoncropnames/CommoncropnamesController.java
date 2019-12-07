858e1a886db03085b705ca3f4bf874d801af1b2e
Krystian Madra <madrakrystian@gmail.com>
2019-12-07 09:59:36 +0100

    [IBDR-145] Refactor commoncropnames

--- brapi/src/main/java/org/planth_pheno_analytics/brapi/api/commoncropnames/CommoncropnamesController.java
+++ brapi/src/main/java/org/planth_pheno_analytics/brapi/api/commoncropnames/CommoncropnamesController.java
@@ -3,13 +3,10 @@ package org.planth_pheno_analytics.brapi.api.commoncropnames;
 import org.planth_pheno_analytics.brapi.annotation.BrAPIController;
 import org.planth_pheno_analytics.brapi.api.BrAPIResponse;
 import org.planth_pheno_analytics.brapi.api.response.BrAPIDetailResponse;
-import org.planth_pheno_analytics.brapi.api.response.metadata.Pagination;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @BrAPIController
 public class CommoncropnamesController {
@@ -23,10 +20,6 @@ public class CommoncropnamesController {
     @ResponseStatus(HttpStatus.OK)
     public BrAPIResponse getBrAPICommoncropnames(Pageable pageable) {
         final Page<String> resultPage = commoncropnamesProjectionResources.getPagedCommoncropnames(pageable);
-
-        final Pagination pagination = Pagination.of(resultPage);
-        final List<String> resultData = resultPage.getContent();
-
-        return new BrAPIDetailResponse(pagination, resultData);
+        return new BrAPIDetailResponse(resultPage);
     }
 }
