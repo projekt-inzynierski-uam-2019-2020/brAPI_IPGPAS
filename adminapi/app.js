608484dda81861067ca71b5c32d9bbd53b2e9d6a
Szymon Wojciechowski <szywoj95@gmail.com>
2019-12-08 13:27:19 +0100

    added session, file store, uuid

--- adminapi/app.js
+++ adminapi/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const uuid = require("uuid");
+const session = require("express-session");
+const FileStore = require('session-file-store')(session);
 
 const server = require("./routes/serverRoute");
-const user = require("./routes/userRoute")
+const user = require("./routes/userRoute");
 
 let port = 3000;
 let mongoUrl = "mongodb://admin:admin@db.admin.local:27017/adminapi-db";
@@ -24,9 +27,28 @@ let db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(
+  session({
+    genid: req => {
+      console.log("Inside the session middleware");
+      console.log(req.sessionID);
+      return uuid();
+    },
+    store: new FileStore(),
+    secret: "replace this",
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
 app.use("/servers", server);
 app.use("/users", user);
+
+app.get("/", (req, res) => {
+  console.log('Inside the homepage callback function')
+  console.log(req.sessionID)
+  res.send(`You hit home page!\n`)
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
