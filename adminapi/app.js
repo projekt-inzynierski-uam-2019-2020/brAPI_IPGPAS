0b0f15f51fe5e5528fddbe46fc6d79fc07bd0d59
Szymon Wojciechowski <szywoj95@gmail.com>
2019-12-08 12:33:15 +0100

    added routing for users

--- adminapi/app.js
+++ adminapi/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const server = require("./routes/serverRoute");
+const user = require("./routes/userRoute")
 
 let port = 3000;
 let mongoUrl = "mongodb://admin:admin@db.admin.local:27017/adminapi-db";
@@ -23,7 +24,9 @@ let db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
 app.use("/servers", server);
+app.use("/users", user);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
