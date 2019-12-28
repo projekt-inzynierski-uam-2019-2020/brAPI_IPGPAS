7a27dcad9626353e3b9b44cadf435670b961b75a
Szymon Wojciechowski <szywoj95@gmail.com>
2019-12-28 22:54:14 +0100

    fixes and updates

--- isatab-converter/converter.py
+++ isatab-converter/converter.py
@@ -2,20 +2,44 @@ import os
 import zipfile
 
 
-def extract_files(directory):
-    os.chdir(directory)
-    files_in_directory = os.listdir()
-    zips_in_directory = [
-        x for x in files_in_directory if zipfile.is_zipfile(x)
-    ]
+def extract_zips():
+    zips_in_directory = [x for x in os.listdir() if zipfile.is_zipfile(x)]
 
     for zip_ in zips_in_directory:
         with zipfile.ZipFile(zip_) as item:
-            item.extractall(zip_.replace('.zip', ''))
+            item.extractall()
+
+
+def convert():
+    root_dir = os.getcwd()
+    directories = [x for x in os.listdir() if os.path.isdir(x)]
+
+    for dir_ in directories:
+        os.chdir(dir_)
+
+        isatab_files = os.listdir()
+        isatab_files_dict = {}
+
+        isatab_files_dict['s_files'] = [
+            x for x in isatab_files if x.startswith('s_')
+        ]
+
+        isatab_files_dict['a_files'] = [
+            x for x in isatab_files if x.startswith('a_')
+        ]
+
+        isatab_files_dict['d_files'] = [
+            x for x in isatab_files if x.startswith('d_')
+        ]
+
+        print(isatab_files_dict)
+
+        os.chdir(root_dir)
 
 
 def main():
-    extract_files('.')
+    extract_zips()
+    convert()
 
 
 if __name__ == "__main__":
