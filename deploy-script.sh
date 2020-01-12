91a3edeb83488003f30b7f9aa8ad8e3aa7eedf6d
Szymon Wojciechowski <szywoj95@gmail.com>
2020-01-12 21:33:44 +0100

    command fix

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,3 +1,1 @@
-cd /opt/brAPI_IPGPAS
-sudo git pull
-docker-compose up -d --build
+ssh -i travis-ci-key plantphenoadmin@teamprojectuam.tk "cd /opt/brAPI_IPGPAS && sudo git pull && docker-compose up -d --build" 
