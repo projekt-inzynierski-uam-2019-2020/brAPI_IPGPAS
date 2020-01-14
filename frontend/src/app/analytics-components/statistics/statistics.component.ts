997ab9c34e8812c942c172fc4f2a69420a4dffe0
kubal97 <jakubl1997@interia.pl>
2020-01-14 20:19:20 +0100

    Commit before pull from 162

--- frontend/src/app/analytics-components/statistics/statistics.component.ts
+++ frontend/src/app/analytics-components/statistics/statistics.component.ts
@@ -38,6 +38,8 @@ export class StatisticsComponent implements OnInit {
   variableStudy: string[] = [];
   isStatisticsVisible = false;
   germplasms: GermplasmValues[] = [];
+  selectedStudy = null;
+
 
   numberofValues = [0, 0, 0, 0, 0, 0];
 
