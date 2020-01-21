331e49ac49f8680cb97cb1360a5e0f6e5b675d65
kubal97 <jakubl1997@interia.pl>
2020-01-21 19:22:56 +0100

    Delete wrong status page

--- frontend/src/app/app-routing.ts
+++ frontend/src/app/app-routing.ts
@@ -6,7 +6,6 @@ import {AdminMainPageComponent} from './admin-main-page/admin-main-page.componen
 import {TrialComponent} from './analytics-components/trial/trial.component';
 import {TrialsResolve} from './call-services/trial/trials-resolve.service';
 import {VariablesComponent} from './analytics-components/variables/variables.component';
-import {ServerStatusComponent} from './server-status/server-status.component';
 
 
 const routes: Routes = [
@@ -16,7 +15,6 @@ const routes: Routes = [
   {path: 'admin/page', component: AdminMainPageComponent},
   {path: 'servers/trial', component: TrialComponent, resolve: {dataResolve: TrialsResolve}},
   {path: 'servers/variable', component: VariablesComponent},
-  {path: 'status', component: ServerStatusComponent},
   {path: '', component: HomePageComponent},
 
   {path: '**', component: HomePageComponent}
--- frontend/src/app/server-page/server-page.component.ts
+++ frontend/src/app/server-page/server-page.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {ServersFetchingService} from '../services/servers-service/servers-fetching.service';
-import {Server} from '../services/servers-service/servers';
-import {Globals} from '../globals';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-server-page',
@@ -10,43 +6,14 @@ import {Router} from '@angular/router';
   styleUrls: ['./server-page.component.scss']
 })
 export class ServerPageComponent implements OnInit {
-  globals: Globals;
-
-  servers: Server[];
-  serverService: ServersFetchingService;
-  serverCheckboxes = [];
-
-  isLoading = true;
 
 
   ngOnInit() {
-    this.serverService.getAllServers()
-      .subscribe(data => {
-        this.servers = data;
-        for (const server of this.servers) {
-          this.serverCheckboxes.push({value: server.ipAddress, selected: false});
-        }
-        this.isLoading = false;
-      });
+
   }
 
-  constructor(serverService: ServersFetchingService, globals: Globals, private router: Router) {
-    this.serverService = serverService;
-    this.globals = globals;
-  }
+  constructor() {
 
-
-  public setSelectedCheckboxes() {
-    this.globals.selectedServers = [];
-
-    const result = this.serverCheckboxes.filter(checkbox => checkbox.selected)
-      .map(checkbox => checkbox.value);
-
-    for (const value of result) {
-      this.globals.selectedServers.push(value);
-    }
-
-    this.globals.selectedServers.length > 0 ? this.router.navigate(['/servers/trial']) : alert('You have to select server first.');
   }
 
 }
